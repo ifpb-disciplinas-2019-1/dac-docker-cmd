f8a542ca0f9c4ecda1f2cabf3f1c61b894b0e96f
Ricardo Job <sousajob@gmail.com>
2019-05-09 08:08:58 -0300

    Classe utilizada para a atividade

---
+++ Principal.java
@@ -0,0 +1,13 @@
+class Principal {
+    public static void main(String... args) {
+        System.out.println("oi, eu sou o Docker!!!!!");
+        while (true) {
+            try {
+                Thread.sleep(15000);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+
+}
